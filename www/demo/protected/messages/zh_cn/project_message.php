fa54670f0b23b1b7f5381c4ed00b3c3351518c4f
Y <publlicworkspace@163.com>
2015-08-13 17:43:56 +0800

    logo

---
+++ www/demo/protected/messages/zh_cn/project_message.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Y
+ * Date: 2015/8/13 0013
+ * Time: 16:59
+ */
+
+return array (
+    'Home' => '首页',
+    'About' => '关于',
+    'Contact' => '联系我们',
+    'Logout' => '退出',
+    'Login' => '登录',
+    'username' => '用户名',
+    'password' => '密码',
+    'rememberMe' => '记住我',
+    'Welcome to' => '欢迎',
+);
